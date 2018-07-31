27183829750873ceeeadd89dcc7256debc2b035f
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2018-08-01 00:42:02 +0200

    Update router.ts

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,7 +1,5 @@
 import { welcome, hello } from "../handlers";
 
-const routes = [];
-
 export default function router(fastify, opts, next) {
   fastify.get("/", welcome);
   fastify.get("/api", hello);
