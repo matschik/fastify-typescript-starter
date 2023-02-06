fb9d6a965cb340ce9bda9a3f6e4cc2cbb7852b84
DanCodes <dan@dancodes.online>
2023-02-06 13:28:45 +0100

    fix(fastify): deprecated .listen(port) requires change

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from "./app";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3006;
 
-app.listen(FASTIFY_PORT);
+app.listen({ port: FASTIFY_PORT });
 
 console.log(`🚀  Fastify server running on port ${FASTIFY_PORT}`);
 console.log(`Route index: /`);
