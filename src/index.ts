4cefd15a4889de434beb2345e73f2c5b8b8c7e3d
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2023-07-29 23:38:11 +0700

    a new step further

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3006;
 
 app.listen({ port: FASTIFY_PORT });
 
-console.log(`🚀  Fastify server running on port ${FASTIFY_PORT}`);
+console.log(`🚀  Fastify server running on port http://localhost:${FASTIFY_PORT}`);
 console.log(`Route index: /`);
 console.log(`Route user: /api/v1/user`);
